5f50f4f0ee07a2c95d78588c440ae607968ef1a0
nijiyamaharjan <nijiyamn@gmail.com>
2024-04-16 21:16:51 +0545

    added modals

--- components/Details.js
+++ components/Details.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native'; 
+import { View, Text, StyleSheet, ScrollView, Image, Modal, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
- // Import ScrollView
 import { FontAwesome } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 
 function DetailsScreen({ navigation }) {
   const [data, setData] = useState([]);
-  
-  
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
     fetchData();
@@ -17,15 +16,12 @@ function DetailsScreen({ navigation }) {
   const fetchData = async () => {
     try {
       const token = await AsyncStorage.getItem('token');
-      console.log(token)
-      const response = await fetch('http://192.168.101.10:8000/api/images/',
-    {
-      headers: {
-        'Authorization': `Token ${token}`, // Include token in headers
-        'Content-Type': 'application/json',
-      },
-
-    });
+      const response = await fetch('http://192.168.101.10:8000/api/images/', {
+        headers: {
+          'Authorization': `Token ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
       const jsonData = await response.json();
       setData(jsonData);
     } catch (error) {
@@ -33,122 +29,131 @@ function DetailsScreen({ navigation }) {
     }
   };
 
+  const openModal = (imageUri) => {
+    setSelectedImage(imageUri);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+    setModalVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.headerText}>  Summarize Your Reports</Text>
-      <View style={styles.row}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Text style={styles.text}>Upload Report</Text>
-            <IconButton
-              title={''}
-              onPress={() => navigation.navigate('UploadImage')}
-              icon={<FontAwesome name="photo" size={24} color="black" />}
-            />
-          </View>
-        </View>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Text style={styles.text}>Scan Report</Text>
-            <IconButton
-              title={''}
-              onPress={() => console.log('IconButton pressed')}
-              icon={<Entypo name="camera" size={24} color="black" />}
-            />
-          </View>
-        </View>
+      <Text style={styles.headerText}>Summarize Your Reports</Text>
+
+      <View style={styles.optionsContainer}>
+        <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('UploadImage')}>
+          <FontAwesome name="photo" size={24} color="black" />
+          <Text style={styles.optionText}>Upload Report</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.option} onPress={() => console.log('Scan report pressed')}>
+          <Entypo name="camera" size={24} color="black" />
+          <Text style={styles.optionText}>Scan Report</Text>
+        </TouchableOpacity>
       </View>
 
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 20, alignItems: 'center', marginTop: 20 }}>
+      <View style={styles.latestReportsContainer}>
         <Text style={styles.headerText}>Latest Reports</Text>
-        <Text>View All</Text>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <View style={styles.reportsList}>
+            {data.map((item, index) => (
+              <TouchableOpacity key={index} onPress={() => openModal(item.picture)}>
+                <Image style={styles.reportImage} source={{ uri: item.picture }} />
+              </TouchableOpacity>
+            ))}
+          </View>
+        </ScrollView>
       </View>
 
-      <ScrollView horizontal style={styles.scrollContainer}>
-        <View style={styles.row}>
-          {data.map((item, index) => (
-            <View style={styles.card} key={index}>
-              <View style={styles.header}>
-              
-                <Text style={styles.text}>{item.id}</Text>
-                <Image
-                  style={{ width: 180, height: 200 }} 
-                  source={{ uri: item.picture }} 
-                />
-                
-                <IconButton
-                  title={''}
-                  onPress={() => console.log('IconButton pressed')}
-                />
-              </View>
-            </View>
-          ))}
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalContainer}>
+          <TouchableOpacity style={styles.modalBackground} onPress={closeModal} />
+          <View style={styles.modalContent}>
+            <Image style={styles.modalImage} source={{ uri: selectedImage }} />
+          </View>
         </View>
-      </ScrollView>
+      </Modal>
     </View>
   );
 }
 
 export default DetailsScreen;
 
-const IconButton = ({ title, onPress, icon }) => (
-  <TouchableOpacity style={styles.button} onPress={onPress}>
-    <Text>{title}</Text>
-    {icon}
-  </TouchableOpacity>
-);
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'left',
-    alignItems: 'left',
     backgroundColor: '#f5f5f5',
+    paddingHorizontal: 16,
+    paddingTop: 20,
   },
   headerText: {
     fontFamily: 'custom-font',
     fontSize: 30,
     fontWeight: 'bold',
-    paddingTop: 100,
-    justifyContent: 'center',
     color: '#333',
+    marginBottom: 20,
   },
-  text: {
-    fontSize: 17,
-    color: '#444444',
-    textAlign: 'center',
-  },
-  row: {
+  optionsContainer: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
   },
-  card: {
-    flex: 1,
+  option: {
     backgroundColor: 'white',
     borderRadius: 15,
-    padding: 8,
-    margin: 16,
-    shadowColor: 'black',
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 6,
-    elevation: 14,
+    padding: 16,
     justifyContent: 'center',
     alignItems: 'center',
-    maxWidth: '50%',
+    width: '48%',
   },
-  header: {
-    marginBottom: 16,
-    alignItems: 'center',
+  optionText: {
+    marginTop: 8,
+    fontSize: 16,
+    color: '#444444',
   },
-  button: {
-    alignItems: 'center',
+  latestReportsContainer: {
+    marginBottom: 20,
+  },
+  reportsList: {
+    flexDirection: 'row',
+  },
+  reportImage: {
+    width: 180,
+    height: 200,
+    marginRight: 10,
+    borderRadius: 10,
+  },
+  modalContainer: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
-  scrollContainer: {
-    marginTop: 20, // Adjust as needed
-    marginBottom: 20, // Adjust as needed
+  modalBackground: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    borderRadius: 15,
+    padding: 20,
+    alignItems: 'center',
+    elevation: 5,
+  },
+  modalImage: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
   },
 });
